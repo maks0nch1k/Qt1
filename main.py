33917dd43bbd47edf5650542dfcd3c956c0ed2aa
maks0nch1k <maksimf647@gmail.com>
2023-01-18 19:14:54 +0300

    commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle("Git и желтые окружности")
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor("yellow"))
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        size = random.randint(0, 300)
+        qp.drawEllipse(QPoint(400, 300), size, size)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
